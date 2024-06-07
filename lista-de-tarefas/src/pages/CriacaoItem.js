be67612b776a92cba59b64085e9e2b48f49fbc41
gabriel-d-sona <gdsona@hotmail.com>
2024-06-07 14:47:01 -0300

    Iniciando Criacao Item

--- lista-de-tarefas/src/pages/CriacaoItem.js
+++ lista-de-tarefas/src/pages/CriacaoItem.js
@@ -4,9 +4,8 @@ class CriacaoLista extends React.Component {
     render () {
         return (
             <div>
-                <h1 className="titleCriacao">Criando Lista</h1>
+                <h1 className="titleCriacao">Criando Item</h1>
                 <form className="formCriacao">
-
                 </form>
             </div>
         )
