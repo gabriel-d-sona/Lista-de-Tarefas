95fd77dae35e421f15c759a4bfc220eed23d8a4f
gabriel-d-sona <gdsona@hotmail.com>
2024-06-07 14:35:37 -0300

    pages criadas, criando criacao de listas

---
+++ lista-de-tarefas/src/pages/CriacaoLista.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class CriacaoLista extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1 className="titleCriacao">Criando Lista</h1>
+                <form className="formCriacao">
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CriacaoLista;
