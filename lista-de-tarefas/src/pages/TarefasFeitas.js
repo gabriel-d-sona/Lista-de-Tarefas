f35f3eff36fef90ba5cedb272b5c734226482ed7
gabriel-d-sona <gdsona@hotmail.com>
2024-06-11 14:52:09 -0300

    tabela tarefas feitas pronta

--- lista-de-tarefas/src/pages/TarefasFeitas.js
+++ lista-de-tarefas/src/pages/TarefasFeitas.js
@@ -1,17 +1,42 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import TableTarefasFeitas from "../componentes/TableTarefasFeitas";
 
 class TarefasFeitas extends React.Component {
     state = {
         navigate: false,
+        historicoArr: [],
+    }
+
+    componentDidMount() {
+        const storedData = JSON.parse(localStorage.getItem("historicoArr")) || [];
+        this.setState({ historicoArr: storedData });
     }
 
     handleBackClick = () => {
         this.setState({ navigate: true });
     }
 
+    handleReturn = (index) => {
+        const newArr = [...this.state.historicoArr];
+        const returnItem = newArr.splice(index, 1)[0];
+        this.setState({ historicoArr: newArr });
+        localStorage.setItem("historicoArr", JSON.stringify(newArr));
+
+        const meuArr = JSON.parse(localStorage.getItem("meuArr")) || [];
+        meuArr.push(returnItem);
+        localStorage.setItem("meuArr", JSON.stringify(meuArr));
+    }
+
+    handleDelete = (index) => {
+        const newArr = [...this.state.historicoArr];
+        newArr.splice(index, 1);
+        this.setState({ historicoArr: newArr });
+        localStorage.setItem("historicoArr", JSON.stringify(newArr));
+    }
+
     render() {
-        const { navigate } = this.state;
+        const { navigate, historicoArr } = this.state;
         if (navigate) {
             return <Navigate to="/" />
         }
@@ -26,6 +51,11 @@ class TarefasFeitas extends React.Component {
                         Voltar
                     </button>
                 </div>
+                <TableTarefasFeitas
+                    historicoArr={historicoArr}
+                    onDelete={this.handleDelete}
+                    onReturn={this.handleReturn}
+                 />
             </div>
         )
     }
