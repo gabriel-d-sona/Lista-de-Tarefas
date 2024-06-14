84475f586b44c986a9a9efa0c9369ed4ecfac321
gabriel-d-sona <gdsona@hotmail.com>
2024-06-14 15:21:59 -0300

    finalizando front

--- lista-de-tarefas/src/pages/CriacaoItem.js
+++ lista-de-tarefas/src/pages/CriacaoItem.js
@@ -61,76 +61,78 @@ class CriacaoItem extends React.Component {
         return (
             <div>
                 <h1 className="titleCriacao">Criando Item</h1>
-                <form className="formCriacao">
-                    <div className="inputsCriacao">
-                        <div className="divForm">
-                            <label>Título do Item</label>
-                            <input
-                                id="inputFormCriacao"
-                                type="text"
-                                name="inputFormCriacao"
-                                value={inputFormCriacao}
-                                onChange={this.handleChange}
-                            />
+                <div className="containerCriacao">
+                    <form className="formCriacao">
+                        <div className="inputsCriacao">
+                            <div className="divForm">
+                                <label>Título do Item</label>
+                                <input
+                                    id="inputFormCriacao"
+                                    type="text"
+                                    name="inputFormCriacao"
+                                    value={inputFormCriacao}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="divForm">
+                                <label>Categoria</label>
+                                <select
+                                    name="select1"
+                                    id="select1"
+                                    value={select1}
+                                    onChange={this.handleChange}
+                                >
+                                    <option value="teste1">Teste1</option>
+                                    <option value="teste2">Teste2</option>
+                                </select>
+                            </div>
+                            <div className="divForm">
+                                <label>Nível de Importância</label>
+                                <select
+                                    name="select2"
+                                    id="select2"
+                                    value={select2}
+                                    onChange={this.handleChange}
+                                >
+                                    <option value="baixo">Baixo</option>
+                                    <option value="medio">Médio</option>
+                                    <option value="alto">Alto</option>
+                                </select>
+                            </div>
                         </div>
-                        <div className="divForm">
-                            <label>Categoria</label>
-                            <select
-                                name="select1"
-                                id="select1"
-                                value={select1}
+                        <div className="descricao">
+                            <label>Descrição</label>
+                            <br />
+                            <textarea
+                                id="descricao"
+                                placeholder="Descrição"
+                                rows="6"
+                                className="descricao"
+                                style={{ width: "26em", height: "10em" }}
+                                name="descricao"
+                                value={descricao}
                                 onChange={this.handleChange}
+                            ></textarea>
+                        </div>
+                        <div className="btns">
+                            <button
+                                id="btn-adicionar"
+                                name="btn-adicionar"
+                                disabled={btnDisabled}
+                                onClick={this.handleAddClick}
                             >
-                                <option value="teste1">Teste1</option>
-                                <option value="teste2">Teste2</option>
-                            </select>
-                        </div>
-                        <div className="divForm">
-                            <label>Nível de Importância</label>
-                            <select
-                                name="select2"
-                                id="select2"
-                                value={select2}
-                                onChange={this.handleChange}
+                                Adicionar
+                            </button>
+                            <button
+                                id="btn-cancelar"
+                                name="btn-cancelar"
+                                onClick={this.handleCancelClick}
                             >
-                                <option value="baixo">Baixo</option>
-                                <option value="medio">Médio</option>
-                                <option value="alto">Alto</option>
-                            </select>
+                                Cancelar
+                            </button>
                         </div>
-                    </div>
-                    <div className="descricao">
-                        <label>Descrição</label>
-                        <br />
-                        <textarea
-                            id="descricao"
-                            placeholder="Descrição"
-                            rows="6"
-                            className="descricao"
-                            style={{ width: "26em", height: "10em" }}
-                            name="descricao"
-                            value={descricao}
-                            onChange={this.handleChange}
-                        ></textarea>
-                    </div>
-                    <div className="btns">
-                        <button
-                            id="btn-adicionar"
-                            name="btn-adicionar"
-                            disabled={btnDisabled}
-                            onClick={this.handleAddClick}
-                        >
-                            Adicionar
-                        </button>
-                        <button
-                            id="btn-cancelar"
-                            name="btn-cancelar"
-                            onClick={this.handleCancelClick}
-                        >
-                            Cancelar
-                        </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         )
     }
